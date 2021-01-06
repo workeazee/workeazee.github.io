d4a739c2768a6dddc6b6addc227be29218f8c510
abaheti <Anurag.BAHETI@amadeus.com>
2021-01-07 01:12:46 +0530

    Formatted code

---
+++ js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    // check if scroll event happened
+    if ($(document).scrollTop() > 10) {
+      // check if user scrolled more than 50 from top of the browser window
+      $(".navbar").addClass("scrolled");
+    } else {
+      $(".navbar").removeClass("scrolled");
+    }
+  });
+});
